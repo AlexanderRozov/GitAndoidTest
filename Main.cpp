b020a8eb98e31e925156b613d20b73aa39c61a83
Alex Rozov <fog545@mail.ru>
2022-04-17 14:20:10 +0400

    Test commit

---
+++ Main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fog54 on 17.04.2022.
+//
+
+#include "Main.h"
---
+++ Main.h
@@ -0,0 +1,16 @@
+//
+// Created by fog54 on 17.04.2022.
+//
+
+#ifndef GITANDOIDTEST_MAIN_H
+#define GITANDOIDTEST_MAIN_H
+
+
+
+class Main {
+
+};
+
+
+
+#endif //GITANDOIDTEST_MAIN_H
